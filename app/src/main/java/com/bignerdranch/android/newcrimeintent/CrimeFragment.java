155eedd2f1a256dd67a52ced068351a7523e9697
hanvit7 <hanvit7@gmail.com>
2016-08-21 23:50:24 +0900

    p271

--- app/src/main/java/com/bignerdranch/android/newcrimeintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/newcrimeintent/CrimeFragment.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.newcrimeintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -20,6 +21,8 @@ import java.util.UUID;
  */
 public class CrimeFragment extends Fragment {
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
@@ -71,7 +74,14 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = (Button) view.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                DataPickerFragment dialog = new DataPickerFragment();
+                dialog.show(fragmentManager, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
