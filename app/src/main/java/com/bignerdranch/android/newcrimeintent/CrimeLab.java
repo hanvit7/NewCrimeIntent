7902bf35bd877b890ad93cf0bab0f974b8219c96
hanvit7 <hanvit7@gmail.com>
2016-08-22 17:26:32 +0900

    p297

--- app/src/main/java/com/bignerdranch/android/newcrimeintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/newcrimeintent/CrimeLab.java
@@ -23,12 +23,16 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("범 죄 #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
+//        for (int i = 0; i < 100; i++) {
+//            Crime crime = new Crime();
+//            crime.setTitle("범 죄 #" + i);
+//            crime.setSolved(i % 2 == 0);
+//            mCrimes.add(crime);
+//        }
+    }
+
+    public void addCrime(Crime crime){
+        mCrimes.add(crime);
     }
 
     public List<Crime> getCrimes() {
--- app/src/main/java/com/bignerdranch/android/newcrimeintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/newcrimeintent/SingleFragmentActivity.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by VIT_HOME on 2016-08-18.
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
